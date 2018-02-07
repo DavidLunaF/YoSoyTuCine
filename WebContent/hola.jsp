8046c95c524c6af630781fd2b332496f142d0d82
Yura <yura.turlay22@gmail.com>
2018-02-07 11:37:02 +0100

    Primer comit
    Creacion de la estructura base del proyecto

---
+++ WebContent/hola.jsp
@@ -0,0 +1,10 @@
+<%@page import="java.util.Date"%>
+<html>
+<head></head>
+<body>
+
+<h1>Hola mundo</h1>
+<%= new Date() %>
+</body>
+
+</html>
---
+++ src/Controladores/ServletGeneral.java
@@ -0,0 +1,83 @@
+package Controladores;
+import java.sql.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+
+
+/**
+ * Servlet implementation class ServletGeneral
+ */
+@WebServlet("/ServletGeneral")
+public class ServletGeneral extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	//Definir el DataSource
+	
+	@Resource(name="jdbc/Peliculas")
+	
+	//almacenamos el pool de conexiones
+	private DataSource miPool;
+	
+	
+	
+	
+	
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletGeneral() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//Crear el objeto printWriter
+		
+		PrintWriter salida=response.getWriter();
+		response.setContentType("text/plain");
+		
+		
+		//Crear la conexion a  la base de datos
+		
+		Connection miConexion=null;
+		Statement miStattement=null;
+		ResultSet miResulset=null;
+		
+		try {
+			miConexion=miPool.getConnection();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
